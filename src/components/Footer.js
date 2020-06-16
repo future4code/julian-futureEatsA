5aaa5991538cf5d0c578c6019a5fd740f564bf8f
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-16 13:14:31 -0300

    colocando rotas no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,32 @@ import HomeIcon from '@material-ui/icons/Home';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import { useHistory } from "react-router-dom";
 
 
 const Footer = () => {
+    const history = useHistory();
+
+    const goToHome = () => {
+        history.push("/home/feedPage");
+    }
+    const goToCart = () => {
+        history.push("/cart");
+    }
+    const goToProfile = () => {
+        history.push("/profile");
+    }
     return (
             <ContainerFooter>
-                <IconButton>
+                <IconButton onClick={goToHome}>
                     <HomeIcon
                         style={{ fontSize: 40 }} />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={goToCart}>
                     <ShoppingCartIcon
                         style={{ fontSize: 40 }} />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={goToProfile}>
                     <PersonIcon
                         style={{ fontSize: 40 }} />
                 </IconButton>
