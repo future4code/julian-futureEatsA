80db17d87ff7c60b938119f00e9f48b3e1182600
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-16 23:13:30 -0300

    criado style.js alteracao no styled-component

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -1,72 +1,12 @@
 import React, { useState } from 'react'
-import { Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core'
-import styled from 'styled-components'
+import {FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-
-
-const CartContainer = styled.div`
-border: 1px solid black;
-width: 360px; 
-height: 640px;
-margin: 30px;
-
-`
-const CardAdress = styled.div`
-width: 360px; 
-height: 76px;
-background-color: #eeeeee;
-
-`
-const TitleAdress = styled.p`
-color: #b8b8b8;
-padding: 16px 16px 0 16px;
-`
-
-const ButtonConfirm = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 328px;
-height: 42px;
-border-radius: 2px;
-background-color: rgba(92, 182, 70, 0.5);
-margin-left: 16px;
-`
-const DeliveryAddress = styled.div`
-color: #000000;
-font-family: Roboto;
-font-size: 16px;
-margin: 8px 16px 16px 16px;
-`
-const ItensCartContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-
-`
-const Frete = styled.p`
-display: flex;
-justify-content: flex-end;
-margin: 45px 16px 0px 0px;
-
-`
-const SubTotalContent = styled.div`
-display:flex;
-justify-content: space-between;
-margin: 16px;
-`
-const ValueTotal = styled.p`
-color: #5cb646;
-`
-const FormaDePagamento = styled.div`
-
-margin: 10px 16px 0px 16px;
-
-`
-
+import { CartContainer, CardAdress, TitleAdress, DeliveryAddress, ItensCartContainer, Frete, SubTotalContent, ValueTotal, FormaDePagamento, ButtonConfirm } from './style'
+import { useHistory } from 'react-router-dom'
 
 const CartPage = ()=>{
+    const history = useHistory();
     const [value, setValue] = useState('Dinheiro');
 
     const handleChange = (event) =>{
---
+++ src/pages/CartPage/style.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const CartContainer = styled.div`
+width: 100vw; 
+height: 100vh;
+flex-direction: column;
+align-items:center;
+`
+export const CardAdress = styled.div`
+width: 100vw; 
+height: 76px;
+background-color: #eeeeee;
+`
+export const TitleAdress = styled.p`
+color: #b8b8b8;
+padding: 16px 16px 0 16px;
+`
+
+export const ButtonConfirm = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 328px;
+height: 42px;
+border-radius: 2px;
+background-color: rgba(92, 182, 70, 0.5);
+margin-left: 16px;
+`
+export const DeliveryAddress = styled.div`
+color: #000000;
+font-family: Roboto;
+font-size: 16px;
+margin: 8px 16px 16px 16px;
+`
+export const ItensCartContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const Frete = styled.p`
+display: flex;
+justify-content: flex-end;
+margin: 45px 16px 0px 0px;
+`
+export const SubTotalContent = styled.div`
+display:flex;
+justify-content: space-between;
+margin: 16px;
+`
+export const ValueTotal = styled.p`
+color: #5cb646;
+`
+export const FormaDePagamento = styled.div`
+margin: 10px 16px 0px 16px;
+`
