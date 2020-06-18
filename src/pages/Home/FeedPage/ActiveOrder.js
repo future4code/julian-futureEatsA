568b9a5c367d736fc8f5cf5a5dfc1221d32f08af
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-18 17:16:35 -0300

    Pedido em andamento finalizado

--- src/pages/Home/FeedPage/ActiveOrder.js
+++ src/pages/Home/FeedPage/ActiveOrder.js
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import CloseIcon from '@material-ui/icons/Close';
 import axios from "axios"
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const Container = styled.div`
     width: 100vw;
-    height: 22vh;
+    height: ${({ aparece }) => (aparece ? '22vh' : '8vh')};
     background-color: #5cb646ed;
     display: flex;
     position: fixed;
@@ -16,6 +17,7 @@ const Container = styled.div`
     align-items:center;
     padding-right:5vw;
     padding-left:5vw;
+    transition: 0.5s;
 `
 
 const ContainerContent = styled.div`
@@ -23,20 +25,30 @@ const ContainerContent = styled.div`
     display: flex;
     flex-direction:column;
     justify-content: space-between;
-    height: 100%;
+    height: ${({ aparece }) => (aparece ? '100%' : 'fit-content')};
     padding-top:10%;
     padding-bottom:10%;
+    transition: 0.5s;
 
 `
 
 const Titulo = styled.h3`
     color:white;
     font-weight: normal;
+    transition: 0.5s;
 
 `
 
 const Restaurant = styled.h3`
     font-weight: normal;
+    display: ${({ aparece }) => (aparece ? 'block' : 'none')};
+    transition: 0.5s;
+
+`
+
+const Preco = styled.h3`
+    display: ${({ aparece }) => (aparece ? 'block' : 'none')};
+    transition: 0.5s;
 
 `
 
@@ -46,7 +58,7 @@ const CloseIconContainer = styled.div`
 
 const ActiveOrder = () => {
     const [activeOrder, setActiveOrder] = useState({restaurantName:'', totalPrice:''})
-
+    const [mostraTexto, setMostraTexto] = useState(true)
 
     const pegaPedido = () => {
         axios
@@ -62,7 +74,6 @@ const ActiveOrder = () => {
                 if(response.data.order !== null){
                 setActiveOrder(response.data.order)                    
                 }
-
             })
             .catch((error) => {
                 alert(error.message)
@@ -73,16 +84,21 @@ const ActiveOrder = () => {
         pegaPedido()
     }, [])
 
+    const alteraStatus = () =>{
+        setMostraTexto(!mostraTexto)
+    }
+
     return (
-        <Container>
-            <AccessTimeIcon style={{ fontSize: 40 }} />
-            <ContainerContent>
-                <Titulo>Pedido em andamento</Titulo>
-                <Restaurant>{activeOrder.restaurantName}</Restaurant>
-                <h3>SUBTOTAL R${activeOrder.totalPrice}</h3>
+        <Container aparece={mostraTexto}>
+            {mostraTexto ? (<AccessTimeIcon style={{ fontSize: 40 }} />) : (<></>)}
+            
+            <ContainerContent >
+                <Titulo aparece={mostraTexto}>Pedido em andamento</Titulo>
+                <Restaurant aparece={mostraTexto}>{activeOrder.restaurantName}</Restaurant>
+                <Preco aparece={mostraTexto}>SUBTOTAL R${activeOrder.totalPrice}</Preco>
             </ContainerContent>
-            <CloseIconContainer>
-                <CloseIcon style={{ fontSize: 30 }} />
+            <CloseIconContainer onClick={alteraStatus}>
+                {mostraTexto ? (<CloseIcon style={{ fontSize: 30 }} />) : (<ExpandLessIcon style={{ fontSize: 30 }} />)}
             </CloseIconContainer>
         </Container>
     )
